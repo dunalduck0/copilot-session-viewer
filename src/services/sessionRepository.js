d95c6cc3d0c0a6b3bb6b8d71b2a386cdcad36ad8
qiaolei81 <qiaolei81@gmail.com>
2026-03-22 13:33:30 +1100

    fix: sort sessions by createdAt descending (newest first)

--- src/services/sessionRepository.js
+++ src/services/sessionRepository.js
@@ -1152,7 +1152,7 @@ class SessionRepository {
    * @private
    */
   _sortByUpdatedAt(sessions) {
-    return sessions.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+    return sessions.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
   }
 }
 
